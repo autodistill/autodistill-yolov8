4e271ed2ddadc928d21f1f0f57c6abd6d5394a20
James Gallagher <jamesg@jamesg.blog>
2023-11-15 14:59:13 -0500

    add device arg

--- autodistill_yolov8/yolov8.py
+++ autodistill_yolov8/yolov8.py
@@ -3,8 +3,6 @@ import supervision as sv
 from ultralytics import YOLO
 import torch
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 class YOLOv8(DetectionTargetModel):
     def __init__(self, model_name):
         self.yolo = YOLO(model_name)
@@ -12,5 +10,5 @@ class YOLOv8(DetectionTargetModel):
     def predict(self, input:str, confidence=0.5) -> sv.Detections:
         return self.yolo(input, conf=confidence)
 
-    def train(self, dataset_yaml, epochs=300, device=DEVICE):
-        self.yolo.train(data=dataset_yaml, epochs=epochs, device=DEVICE)
+    def train(self, dataset_yaml, epochs=300, device="cpu"):
+        self.yolo.train(data=dataset_yaml, epochs=epochs, device=device)
